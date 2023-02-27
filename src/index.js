53ec355a1d4875983fb1c707ac72d8067b895130
R3dFr0g <r3d.fr0g31337@gmail.com>
2023-02-27 14:23:44 +0600

    feat: reverse-int/firstTry

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+  let stringFromNumber = Math.abs(n).toString();
+  let result = stringFromNumber.split('').reverse().join('');
+  return Number(result);
 }
